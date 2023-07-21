b15bab5a6e0146a10868b4878b3645c14fc15959
SnehalJalit <snehaljalit6@gmail.com>
2023-07-21 12:26:57 +0530

    function
    function

---
+++ 04_varnoArg.js
@@ -0,0 +1,34 @@
+// Function with argument and return value
+function addition(arg1, arg2, arg3){
+    console.log('Values are: ', arg1, arg2, arg3);
+    var result = arg1 + arg2 + arg3;
+    return result;
+}
+var res = addition(10, 23, 456);
+console.log('Result is ', addition(10, 23, 456));
+addition(-56, 239, 677);
+
+console.log('result',addition(-56,236,677));
+checkType(null);
+checkType();
+
+// Function with argument and return value
+function addition(arg1, arg2, arg3){
+    console.log('Values are: ', arg1, arg2, arg3);
+    var result = arg1 + arg2 + arg3;
+    return result;
+}
+var res = addition(10, 23, 456);
+console.log('Result is ', res);
+var resultValue = addition(-56, 239, 677);
+console.log('Result is ', resultValue);
+function add(par1,par2 ,par3) {
+    console.log('values are:',par1,par2,par3);
+    var result=par1 +par2 +par3;
+    return result;
+}
+var result =add('GK', 'Pune', 88);
+console.log('Result is ', result);
+var greet = "Good Morning";
+var lengthGreet= greet.length;
+console.log(greet,'Length of String is:', lengthGreet);
