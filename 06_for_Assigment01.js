7429724a23bb76975054e8005603480d7779c6f7
SnehalJalit <snehaljalit6@gmail.com>
2023-07-31 11:13:16 +0530

    Assignment
    Assignment

---
+++ 06_for_Assigment01.js
@@ -0,0 +1,29 @@
+for (let index = 5; index < 16; index++) {
+    console.log(index);
+    
+}
+console.log(`-------------------------------------`);
+for (let i = 50; i >=40; i--) {
+    console.log(i);
+    
+}
+console.log(`-----------------------------------------`);
+for (let index = 1; index <30; index=index+2) {
+    console.log(index); 
+}
+console.log(`------------------------------------------`);
+for (let index = 2; index <21 ; index=index+2) {
+    console.log(index); 
+}
+console.log(`---------------------------------------------`);
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index); 
+}
+console.log(`-----------------------------------------------`);
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index); 
+}
+console.log(`------------------------------------------------`);
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index); 
+}
